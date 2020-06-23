65cb0b0bc44ea30aee6106d4b6da9e7f263e511a
ivine369 <ivinekemuma014@gmail.com>
2020-06-23 11:53:34 +0300

    add a profile place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SearchUserService } from './searchUser.service';
+import { prepareProfile } from 'selenium-webdriver/firefox';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Github-Search';
+  title = 'Git User Search Portal';
+  profile: any;
+  repos: any;
+  username: string;
+
+  constructor(private service: SearchUserService) {
+}
+findProfile() {
+  this.service.updateProfile(this.username);
+  this.service.getuser().subscribe(profile => {
+    this.profile = profile;
+    console.log(profile);
+  });
+
+  this.service.getUserRepos().subscribe(repos => {
+    this.repos = repos;
+    console.log(repos);
+  });
+}
+
 }
