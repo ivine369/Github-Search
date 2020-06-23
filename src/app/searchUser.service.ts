a8b93c986c8e06c1b1bf2911e601748e251bcc0e
ivine369 <ivinekemuma014@gmail.com>
2020-06-23 11:56:44 +0300

    add string for credentials

---
+++ src/app/searchUser.service.ts
@@ -0,0 +1,29 @@
+ 
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable ({
+    providedIn: 'root'
+})
+
+export class SearchUserService {
+
+    private username: string;
+    private clientid='';
+    private clientsecret='';
+
+    constructor(private http: HttpClient) {
+        console.log('Service has already Started.....');
+        this.username = 'ivine369';
+     }
+
+    getuser() {
+        return this.http.get('https://api.github.com/users/' + this.username);
+    }
+    getUserRepos() {
+        return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id=');
+    }
+    updateProfile(username: string) {
+        this.username = username;
+    }
+}
